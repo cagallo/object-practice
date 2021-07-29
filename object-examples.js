6591040ef6f1c41aab3d6dc5bbcda56bb4d201d0
Cesare Gallo <cesaregallo0@gmail.com>
2021-07-28 20:23:31 -0600

    Add dog model

--- object-examples.js
+++ object-examples.js
@@ -2,5 +2,12 @@ var car = {
   name: "Scooby",
   age: 3;,
   isGasConserving: true,
-  work_performed: [tires, oil, air filter]
+  work_performed: ['tires', 'oil', 'air filter']
+}
+
+var dog = {
+  name: "TeddyZ",
+  ageInMonths: 2,
+  isAGoodBoy: true,
+  favoriteFoods: ['chicken', 'beef', 'rice']
 }
